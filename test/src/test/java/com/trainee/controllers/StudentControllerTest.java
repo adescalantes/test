5121178b04883911dee47b8f7726efbb47b983ae
adescalantes <adescalantes@gmail.com>
2019-05-20 00:39:26 -0500

    Unit Tests Mockito

--- test/src/test/java/com/trainee/controllers/StudentControllerTest.java
+++ test/src/test/java/com/trainee/controllers/StudentControllerTest.java
@@ -1,14 +1,60 @@
 package com.trainee.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.trainee.models.Student;
+import com.trainee.repositories.StudentRepository;
+import com.trainee.services.IStudentService;
+import com.trainee.services.implement.StudentServiceImpl;
+
+@RunWith(SpringRunner.class)
 class StudentControllerTest {
 
+	@Mock
+	StudentRepository studentRepository;
+	
+	@Mock
+	IStudentService studentService;
+	
+	@InjectMocks
+	StudentController studentController;
+	
+	private MockMvc mockMvc;
+	
+	@BeforeEach
+	 public void setup(){
+
+        MockitoAnnotations.initMocks(this);
+        
+        mockMvc = MockMvcBuilders.standaloneSetup(studentController).build();
+
+    }
+	
 	@Test
-	void testGetAll() {
-		fail("Not yet implemented");
+	void testGetAll() throws Exception {
+		Student student = new Student();
+		student.setId(1);
+		student.setFirstName("Mock");
+		
+		when(studentRepository.findAll()).thenReturn(Collections.singleton(student));
+		studentRepository.save(student);
+		mockMvc.perform(get("/v1/students")).andDo(print());
 	}
 
 	@Test
